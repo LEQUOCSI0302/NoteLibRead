b09be2ce738d5907cf8db965d731ae1d6f1c6d8a
Si <kinzizuoker0@gmail.com>
2026-05-30 13:30:56 +0700

    add folder spring boot

---
+++ src/main/java/com/app/NoteLibs/NoteLibsApplication.java
@@ -0,0 +1,13 @@
+package com.app.NoteLibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteLibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteLibsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/NoteLibs/NoteLibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.NoteLibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteLibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
